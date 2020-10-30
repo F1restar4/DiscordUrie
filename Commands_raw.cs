3934ab8219c621cee6501107827e81f17e47262b
Alex <firestar@firestar4.com>
2020-10-29 20:49:41 -0500

    More efficient TAP usage

--- Commands_raw.cs
+++ Commands_raw.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -14,32 +10,22 @@ namespace DiscordUrie_DSharpPlus
 	{
 		[Command("raw"), Description("Supplies information about a givin object")]
 		public async Task Raw(CommandContext ctx, DiscordMessage msg)
-		{
-			await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(msg, Formatting.Indented)}\n```");
-		}
+			=> await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(msg, Formatting.Indented)}\n```");
 
 		[Command("raw")]
 		public async Task Raw (CommandContext ctx, DiscordMember member)
-		{
-			await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(member, Formatting.Indented)}\n```");
-		}
+			=> await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(member, Formatting.Indented)}\n```");
 
 		[Command("raw")]
 		public async Task Raw (CommandContext ctx, DiscordChannel channel)
-		{
-			await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(channel, Formatting.Indented)}\n```");
-		}
+			=> await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(channel, Formatting.Indented)}\n```");
 
 		[Command("raw")]
-		public async Task Raw (CommandContext ctx, DiscordRole role)
-		{
-			await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(role, Formatting.Indented)}\n```");
-		}
+		public async Task Raw (CommandContext ctx, DiscordRole role) 
+			=> await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(role, Formatting.Indented)}\n```");
 
 		[Command("raw")]
 		public async Task Raw(CommandContext ctx, DiscordGuild guild)
-		{
-			await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(guild, Formatting.Indented)}\n```");
-		}
+			=> await ctx.RespondAsync($"```\n{JsonConvert.SerializeObject(guild, Formatting.Indented)}\n```");
 	}
 }
